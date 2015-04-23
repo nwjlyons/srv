002849a250fc2b50be864eb8df032a2caa7ca1e0
Neil Lyons <nwjlyons@googlemail.com>
2015-04-23 20:48:26 +0900

    Filepath can be specified.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 var port = flag.String("p", "8000", "Port to run server on.")
@@ -12,8 +13,14 @@ var port = flag.String("p", "8000", "Port to run server on.")
 func main() {
 	flag.Parse()
 
+	dir, err := filepath.Abs(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Serving directory %s on port %s.\n", dir, *port)
 	log.Fatal(
 		http.ListenAndServe(fmt.Sprintf(":%s", *port),
 			http.FileServer(
-				http.Dir("."))))
+				http.Dir(dir))))
 }
